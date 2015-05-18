f9a3f74b44286d4f0886f978a2c44c19416530a1
joni2back <joni2back@gmail.com>
2015-05-18 11:31:36 -0300

    first commit v0.1.0 alpha

---
+++ css.js
@@ -0,0 +1,71 @@
+(function(){
+
+    var formulas = {
+        w: 'width',
+        h: 'height',
+        p: 'padding',
+        m: 'margin',
+        br: 'borderRadius',
+        fs: 'fontSize',
+        fw: 'fontWeight',
+        lh: 'lineHeight',
+
+        mt: 'marginTop',
+        mb: 'marginBottom',
+        ml: 'marginLeft',
+        mr: 'marginRight',
+
+        pt: 'paddingTop',
+        pb: 'paddingBottom',
+        pl: 'paddingLeft',
+        pr: 'paddingRight',
+    }
+   
+    var useFormula = function(className) {
+        var pieces = className.match(/(^[a-z]{1,3})+([0-9pxem\%]{1,5})/);
+        var formula = pieces && formulas[pieces[1]];
+        return formula && {
+            key: formula,
+            value: pieces[2]
+        };
+    };
+
+    var parseElementClasses = function(element) {
+        var classNames = element && element.className && element.className.split(' ') || [];
+        classNames.forEach(function(className) {
+            var style = useFormula(className);
+            style && style.key && (element.style[style.key] = style.value);
+        });
+    }
+
+    var apply = function(scopeElement) {
+        var scope = scopeElement || window.document;
+        var elements = scope.getElementsByTagName('*');
+        for (var i in elements) {
+            var element = elements[i];
+            if (typeof element === 'object') {
+                element.className && parseElementClasses(element);
+                observer.observe(element, { attributes : true, attributeFilter : ['style', 'class'] });
+            }
+        }
+    };
+
+    var observer = new window.MutationObserver(function(mutations) {
+        mutations.forEach(function(mutationRecord) {
+            var element = mutationRecord.target;
+            element.lastClassName !== element.className && parseElementClasses(element);
+            element.lastClassName = element.className;
+        });    
+    });
+
+
+    if (window && window.document) {
+        window.cssjs = {
+            apply: apply
+        }
+        return;
+    }
+
+    throw new Error('Invalid environment');
+
+})(window);
